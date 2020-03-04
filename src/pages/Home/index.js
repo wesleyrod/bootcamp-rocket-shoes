e6dfa33a2beb1e8c6055d01c69a42c46a3c0e4fa
Wesley Rodrigues <carlosjr.if@gmail.com>
2020-03-04 17:24:17 -0300

    #06 - Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/bnn/l_netshoes/2020-02-28/613_trio5.jpg"
+          alt="Tênis Adidas"
+        />
+        <strong>Tênis em promoção</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
