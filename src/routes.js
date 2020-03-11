cb5346aa1103446fb9839413562aa1fa53de2589
Wesley Rodrigues <wesleyrod.br@gmail.com>
2020-03-11 13:44:08 -0300

    #21 - Reactotron + Saga

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Router, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
