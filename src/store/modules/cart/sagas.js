489540d01a3dcc7aa3d50f50292b8d55f01b9e66
Wesley Rodrigues <wesleyrod.br@gmail.com>
2020-03-19 10:21:46 -0300

    #22 - Separando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,10 +1,25 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSucess } from './actions';
+import { addToCartSucess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
-  yield put(addToCartSucess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormated: formatPrice(response.data.price),
+    };
+    yield put(addToCartSucess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
