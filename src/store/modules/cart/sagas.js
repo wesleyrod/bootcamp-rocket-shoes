7687e8b47a95ab6c0ade4f7ad78b14ff6a7db315
Wesley Rodrigues <wesleyrod.br@gmail.com>
2020-03-19 11:09:52 -0300

    #25 - Estoque na alteração

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,7 +1,7 @@
 import { toast } from 'react-toastify';
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSucess, updateAmount } from './actions';
+import { addToCartSucess, updateAmountSuccess } from './actions';
 import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
@@ -20,7 +20,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
     const data = {
@@ -32,4 +32,20 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  // const product = yield select(state => state.find(p => p.id === id));
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+  if (amount > stockAmount) {
+    toast.error('Quantidade indisponível em estoque', 3000);
+    return;
+  }
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
